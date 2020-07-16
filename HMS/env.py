c6256e0998d61dbe75e707e7420bc98c30e8f495
Adedeji Saheed Ademola <sademolaadedeji@gmail.com>
2020-07-16 02:05:14 +0100

    Changed the record model from postgres array field to foreignkeys

--- HMS/env.py
+++ HMS/env.py
@@ -1,4 +1,4 @@
-"""Database Configuration sample
+"""Database Configuration sample"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -8,15 +8,15 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5433'
     }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_name',
-        'USER': 'db_user',
-        'PASSWORD': 'db_pass',
-        'HOST': 'db_host',
-        'PORT': 'db_port'
-    }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db_name',
+#         'USER': 'db_user',
+#         'PASSWORD': 'db_pass',
+#         'HOST': 'db_host',
+#         'PORT': 'db_port'
+#     }
+# }
--- record/models.py
+++ record/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
 from users.models import Patient
 
 class MedicalHistory(models.Model):
@@ -17,13 +16,43 @@ class MedicalHistory(models.Model):
     patient = models.OneToOneField(Patient, on_delete=models.CASCADE)
     createdOn = models.DateTimeField(auto_now_add=True)
     updatedOn = models.DateTimeField(auto_now=True)
-    diseases = ArrayField(models.CharField(max_length=100),blank=True)
-    surgeries = ArrayField(models.CharField(max_length=100), blank=True)
-    vaccinations = ArrayField(models.CharField(max_length=100), blank=True)
-    allergies = ArrayField(models.CharField(max_length=100), blank=True)
-    medications = ArrayField(models.CharField(max_length=100), blank=True)
     bloodgroup = models.CharField(max_length=2, choices=BLOODGROUP)
     genotype = models.CharField(max_length=2, choices=GENOTYPE)
 
     def __str__(self):              # __unicode__ on Python 2
         return "{} {}".format(self.patient.user.lastName, self.patient.user.firstName)
+
+class Disease(models.Model):
+    name = models.CharField(max_length=200)
+    medicalHistory = models.ForeignKey(MedicalHistory, on_delete=models.CASCADE, related_name="disease")
+
+    def __str__(self):
+        return self.name
+
+class Surgery(models.Model):
+    name = models.CharField(max_length=200)
+    medicalHistory = models.ForeignKey(MedicalHistory, on_delete=models.CASCADE, related_name="surgery")
+
+    def __str__(self):
+        return self.name
+
+class Vaccination(models.Model):
+    name = models.CharField(max_length=200)
+    medicalHistory = models.ForeignKey(MedicalHistory, on_delete=models.CASCADE, related_name="vaccination")
+
+    def __str__(self):
+        return self.name
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=200)
+    medicalHistory = models.ForeignKey(MedicalHistory, on_delete=models.CASCADE, related_name="allergy")
+
+    def __str__(self):
+        return self.name
+
+class Medication(models.Model):
+    name = models.CharField(max_length=200)
+    medicalHistory = models.ForeignKey(MedicalHistory, on_delete=models.CASCADE, related_name="medication")
+
+    def __str__(self):
+        return self.name
