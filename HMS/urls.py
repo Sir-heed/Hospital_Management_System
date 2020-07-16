c0794fc7dc5f114787a1580431217a940e77d99c
Adedeji Saheed Ademola <sademolaadedeji@gmail.com>
2020-07-16 02:45:29 +0100

    Previous migrations files deleted as this would generate error because of the array field, database switched to sqlite successfully

--- HMS/urls.py
+++ HMS/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('record/', include('record.urls'))
+    # path('', include('users.urls')),
+    # path('record/', include('record.urls'))
 ]
